b436a8d7e123e4b600d0257c163aaea7fa0ada79
joao carvalho <joaovcarvalho5626@gmail.com>
2018-07-16 18:29:21 -0300

    adicionado o trabalho

---
+++ quezia2/mavenproject1/src/main/java/Cliente.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+    private String codIdentificacao;
+    private String email;
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String tipoCliente;
+
+    /**
+     * @return the codIdentificacao
+     */
+    public String getCodIdentificacao() {
+        return codIdentificacao;
+    }
+
+    /**
+     * @param codIdentificacao the codIdentificacao to set
+     */
+    public void setCodIdentificacao(String codIdentificacao) {
+        this.codIdentificacao = codIdentificacao;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nomeFantasia
+     */
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    /**
+     * @param nomeFantasia the nomeFantasia to set
+     */
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    /**
+     * @return the razaoSocial
+     */
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    /**
+     * @param razaoSocial the razaoSocial to set
+     */
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    /**
+     * @return the tipoCliente
+     */
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    /**
+     * @param tipoCliente the tipoCliente to set
+     */
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+}
---
+++ quezia2/mavenproject1/src/main/java/OrdemServico.java
@@ -0,0 +1,46 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class OrdemServico {
+    private Date data;
+    private double valorTotal;
+
+    /**
+     * @return the data
+     */
+    public Date getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
+}
